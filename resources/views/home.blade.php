7c391d11eac08ad69130d80b265384abb0d01381
Abs-Anik <abubakkarsiddik.cse@gmail.com>
2020-12-20 21:16:56 +0600

    Post page design complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Post</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <h1>Add A Post</h1>
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="title">Enter Post Title</label>
+                            <input type="text" name="title" id="title" class="form-control">
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="form-group">
+                            <label for="description">Enter Post Description</label>
+                            <textarea name="description" id="description" rows="5" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            <select name="category_id" id="category" class="form-control">
+                                @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <input type="submit" class="btn btn-success" value="Publish">
+                    </form>
                 </div>
             </div>
         </div>
